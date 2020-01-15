26845a2088f4e64a5c0d2b4f24f51094f81c3e1b
Robert Herber <robert@herber.me>
2020-01-15 11:06:33 +0100

    Updates

---
+++ precache-manifest.b0f06f40b7ef0238e4447b3f47d604ee.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "2762c546379f1fd8221b",
+    "url": "/react-native-device-info/static/js/app.1b1c77e1.chunk.js"
+  },
+  {
+    "revision": "5b7da254edcb12ad0b2a",
+    "url": "/react-native-device-info/static/js/runtime~app.2a544480.js"
+  },
+  {
+    "revision": "bd99657e2357d8a15e87",
+    "url": "/react-native-device-info/static/js/2.59dae689.chunk.js"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/react-native-device-info/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/react-native-device-info/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/react-native-device-info/expo-service-worker.js"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/react-native-device-info/favicon.ico"
+  },
+  {
+    "revision": "ca0c034a4972a00ad65bff069ddcdc74",
+    "url": "/react-native-device-info/manifest.json"
+  },
+  {
+    "revision": "ef58ff9a14b1871e5232fada458806e5",
+    "url": "/react-native-device-info/apple/icons/icon_180x180.png"
+  },
+  {
+    "revision": "dd0d8974f196283ab21f6bb647882750",
+    "url": "/react-native-device-info/apple/icons/icon_192x192.png"
+  },
+  {
+    "revision": "b370d9dab464db3e849d854262b5d2e5",
+    "url": "/react-native-device-info/apple/icons/icon_512x512.png"
+  },
+  {
+    "revision": "b4c95fe40cc182ee41709b5386a65a60",
+    "url": "/react-native-device-info/apple/splash/icon_1125x2436.png"
+  },
+  {
+    "revision": "24057a53b4b8207ac6128f5a1858a41f",
+    "url": "/react-native-device-info/apple/splash/icon_1136x640.png"
+  },
+  {
+    "revision": "d8bfb76284287658cab36f271d2a2a4a",
+    "url": "/react-native-device-info/apple/splash/icon_1242x2208.png"
+  },
+  {
+    "revision": "46f33d43a6ee160cfb767b7e565d3963",
+    "url": "/react-native-device-info/apple/splash/icon_1242x2688.png"
+  },
+  {
+    "revision": "ff41747f4f3bb2f9a29c2ee494815852",
+    "url": "/react-native-device-info/apple/splash/icon_1334x750.png"
+  },
+  {
+    "revision": "79a7a4869dc12a14e286c382d496dfd0",
+    "url": "/react-native-device-info/apple/splash/icon_1792x828.png"
+  },
+  {
+    "revision": "7e9fc39b1c37a328126323bbe8cd404b",
+    "url": "/react-native-device-info/apple/splash/icon_2208x1242.png"
+  },
+  {
+    "revision": "1fd86c39fec0a11f2f6b61d5a7af0f39",
+    "url": "/react-native-device-info/apple/splash/icon_2436x1125.png"
+  },
+  {
+    "revision": "dbbf9209e64340c9d1b78823b8f8abf2",
+    "url": "/react-native-device-info/apple/splash/icon_2688x1242.png"
+  },
+  {
+    "revision": "df18c0c33e95b50b62f822d65cd9b909",
+    "url": "/react-native-device-info/apple/splash/icon_640x1136.png"
+  },
+  {
+    "revision": "5d0bcf1c916780f7a20f047bb6d8a30c",
+    "url": "/react-native-device-info/apple/splash/icon_750x1334.png"
+  },
+  {
+    "revision": "4ed2cf28de535595662c61587b07aa56",
+    "url": "/react-native-device-info/apple/splash/icon_828x1792.png"
+  },
+  {
+    "revision": "8913e10ec4657d4655a85e14cd2e05f4",
+    "url": "/react-native-device-info/index.html"
+  },
+  {
+    "revision": "b8ccc0611166c869c8bf4ace0f193d59",
+    "url": "/react-native-device-info/static/js/app.1b1c77e1.chunk.js.gz"
+  },
+  {
+    "revision": "abfe3cfb074c04f6ba1de18f35179b65",
+    "url": "/react-native-device-info/static/js/runtime~app.2a544480.js.gz"
+  },
+  {
+    "revision": "16709f23227d220b53e235a942bf292c",
+    "url": "/react-native-device-info/expo-service-worker.js.gz"
+  },
+  {
+    "revision": "94915a4dc0c2471ab48afb9b6282719d",
+    "url": "/react-native-device-info/static/js/2.59dae689.chunk.js.gz"
+  }
+];
